f62147f25af0e050e1fabd4efbfb83cbe6831eae
Pedro Zamorano <pzamorano18@gmail.com>
2023-07-12 02:00:04 -0400

    startcommit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
+
+import 'config/theme/app_theme.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 7).getTheme(),
+      home: const HomeScreen(),
+    );
+  }
+}
